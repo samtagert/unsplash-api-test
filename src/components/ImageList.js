a8f3016e6d7c0ec856ddbf5f786f45ff17e19a48
Sam Tagert <samtagert@gmail.com>
2019-07-30 22:38:27 -0700

    dynamic styling on image grid

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import ImageCard from './ImageCard';
+import './ImageList.css';
 
 const ImageList = props => {
-  const images = props.images.map(({description, id, urls}) => {
-    return <img alt={description} key={id} src={urls.regular} />
+  const images = props.images.map(image => {
+    return <ImageCard key={image.id} image={image} />
   });
 
   return (
-    <div>
+    <div className="image-list">
       {images}
     </div>
     )
